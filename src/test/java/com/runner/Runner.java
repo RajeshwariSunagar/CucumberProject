e81676c636743cd743be683afe9444917e949547
RajeshwariSunagar <rajeshwari.sunagar@gmail.com>
2025-01-10 09:51:23 -0500

    commit4 added tags feature

--- src/test/java/com/runner/Runner.java
+++ src/test/java/com/runner/Runner.java
@@ -1,16 +1,18 @@
 package com.runner;
 
+
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features", glue = {
-		"com.stepDefination" }, monochrome = true, plugin = { "pretty", "html:target/cucumber-html-report.html",
-				"json: JsonReports/cucumber-json-report.json",
-				"junit: XMlReports/cucumber-xml-report.xml" }, dryRun = false)
-
+@RunWith(Cucumber.class) @CucumberOptions( features = "C:\\Users\\rajes\\git\\repository2\\CucumberProj\\src\\test\\resources\\Features", 
+glue = {"com.stepDefination"}, monochrome = true, 
+plugin = { "pretty", "html:target/cucumber-html-report.html", "json:target/cucumber-json-report.json", "junit:target/cucumber-xml-report.xml" }, 
+dryRun = true,
+tags = "@SmokeTest"
+)
+//  
 public class Runner {
 
 }
---
+++ src/test/resources/Features/TagsDemo.feature
@@ -0,0 +1,22 @@
+Feature: Using of tags in Cucumber
+
+  @SmokeTest
+  Scenario: Scenario1
+    Given: This is Scenario1
+
+
+  Scenario: Scenario2
+    Given: This is Scenario2
+
+
+  Scenario: Scenario3
+    Given: This is Scenario3
+
+
+  Scenario: Scenario4
+    Given: This is Scenario4
+
+
+  Scenario: Scenario5
+    Given: This is Scenario5
+
